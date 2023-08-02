99410c87693bf4ba1d4442e8a91046f9a57d2d1f
youjiaqi <jiaqi.you@luttica.com>
2023-08-02 20:46:01 +0800

    构建

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,44 @@ import { SEO } from '@components/common/seo';
 import { LoginMain } from '@components/login/login-main';
 import { LoginFooter } from '@components/login/login-footer';
 import type { ReactElement, ReactNode } from 'react';
+import { getWalletClient } from '@wagmi/core';
+import { useNetwork, useSwitchNetwork } from 'wagmi';
+import { polygon, polygonMumbai } from 'wagmi/chains';
+import { useAccount } from 'wagmi';
+import { useWalletLogin, useActiveProfile } from '@lens-protocol/react-web';
+import { useEffect } from 'react';
+import { MAIN_NETWORK } from '../constants/constant';
 
 export default function Login(): JSX.Element {
+  // const { chain } = useNetwork();
+  //
+  // const { switchNetwork } = useSwitchNetwork();
+  //
+  // const {
+  //   execute: login,
+  //   error: loginError,
+  //   isPending: isLoginPending,
+  // } = useWalletLogin();
+  //
+  // const { isConnected } = useAccount();
+  //
+  // const { data: profile, error, loading: profileLoading } = useActiveProfile();
+  // useEffect(() => {
+  //   if (chain && switchNetwork) {
+  //     const targetNetworkId = MAIN_NETWORK ? polygon.id : polygonMumbai.id;
+  //     if (chain.id !== targetNetworkId) {
+  //       switchNetwork(targetNetworkId);
+  //     }
+  //   }
+  // }, [chain, switchNetwork]);
+  //
+  // const onLoginClick = async () => {
+  //   const walletClient = await getWalletClient();
+  //
+  //   await login({
+  //     address: walletClient.account.address,
+  //   });
+  // };
   return (
     <div className='grid min-h-screen grid-rows-[1fr,auto]'>
       <SEO
