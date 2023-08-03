91c3edd4502bbd7ce7d1f382c56a0f055ae9acb7
dogmanss <975965279@qq.com>
2023-08-03 21:53:23 +0800

    use auth

--- src/lib/context/auth-context.tsx
+++ src/lib/context/auth-context.tsx
@@ -1,46 +1,17 @@
 import { useState, useEffect, useContext, createContext, useMemo } from 'react';
-import {
-  signInWithPopup,
-  GoogleAuthProvider,
-  onAuthStateChanged,
-  signOut as signOutFirebase
-} from 'firebase/auth';
-import {
-  doc,
-  getDoc,
-  setDoc,
-  onSnapshot,
-  serverTimestamp
-} from 'firebase/firestore';
-import { auth } from '@lib/firebase/app';
-import {
-  usersCollection,
-  userStatsCollection,
-  userBookmarksCollection
-} from '@lib/firebase/collections';
 import { getRandomId, getRandomInt } from '@lib/random';
 import type { ReactNode } from 'react';
-import type { User as AuthUser } from 'firebase/auth';
-import type { WithFieldValue } from 'firebase/firestore';
-import type { User } from '@lib/types/user';
 import type { Bookmark } from '@lib/types/bookmark';
-import type { Stats } from '@lib/types/stats';
-
-
-import { useWalletLogin, useActiveProfile } from "@lens-protocol/react-web";
-import { useAccount } from "wagmi";
-import { useNetwork, useSwitchNetwork } from "wagmi";
+import { useWalletLogin, useActiveProfile, useWalletLogout, ProfileOwnedByMe } from "@lens-protocol/react-web";
 import { getWalletClient } from "@wagmi/core";
- 
+
 type AuthContext = {
-  user: User | null;
+  user: ProfileOwnedByMe | null;
   error: Error | null;
   loading: boolean;
-  isAdmin: boolean;
-  randomSeed: string;
   userBookmarks: Bookmark[] | null;
   signOut: () => Promise<void>;
-  signInWithGoogle: () => Promise<void>;
+  signInWithLens: () => Promise<void>;
 };
 
 export const AuthContext = createContext<AuthContext | null>(null);
@@ -52,99 +23,14 @@ type AuthContextProviderProps = {
 export function AuthContextProvider({
   children
 }: AuthContextProviderProps): JSX.Element {
-  const [user, setUser] = useState<User | null>(null);
+
+  const [user, setUser] = useState<ProfileOwnedByMe | null>(null);
   const [userBookmarks, setUserBookmarks] = useState<Bookmark[] | null>(null);
   const [error, setError] = useState<Error | null>(null);
   const [loading, setLoading] = useState(true);
+  
 
-  useEffect(() => {
-    const manageUser = async (authUser: AuthUser): Promise<void> => {
-      const { uid, displayName, photoURL } = authUser;
-
-      const userSnapshot = await getDoc(doc(usersCollection, uid));
-
-      if (!userSnapshot.exists()) {
-        let available = false;
-        let randomUsername = '';
-
-        while (!available) {
-          const normalizeName = displayName?.replace(/\s/g, '').toLowerCase();
-          const randomInt = getRandomInt(1, 10_000);
-
-          randomUsername = `${normalizeName as string}${randomInt}`;
-
-          const randomUserSnapshot = await getDoc(
-            doc(usersCollection, randomUsername)
-          );
-
-          if (!randomUserSnapshot.exists()) available = true;
-        }
-
-        const userData: WithFieldValue<User> = {
-          id: uid,
-          bio: null,
-          name: displayName as string,
-          theme: null,
-          accent: null,
-          website: null,
-          location: null,
-          photoURL: photoURL as string,
-          username: randomUsername,
-          verified: false,
-          following: [],
-          followers: [],
-          createdAt: serverTimestamp(),
-          updatedAt: null,
-          totalTweets: 0,
-          totalPhotos: 0,
-          pinnedTweet: null,
-          coverPhotoURL: null
-        };
-
-        const userStatsData: WithFieldValue<Stats> = {
-          likes: [],
-          tweets: [],
-          updatedAt: null
-        };
-
-        try {
-          await Promise.all([
-            setDoc(doc(usersCollection, uid), userData),
-            setDoc(doc(userStatsCollection(uid), 'stats'), userStatsData)
-          ]);
-
-          const newUser = (await getDoc(doc(usersCollection, uid))).data();
-          setUser(newUser as User);
-        } catch (error) {
-          setError(error as Error);
-        }
-      } else {
-        const userData = userSnapshot.data();
-        setUser(userData);
-      }
-
-      setLoading(false);
-    };
-
-    const handleUserAuth = (authUser: AuthUser | null): void => {
-      setLoading(true);
-
-      if (authUser) void manageUser(authUser);
-      else {
-        setUser(null);
-        setLoading(false);
-      }
-    };
-
-    onAuthStateChanged(auth, handleUserAuth);
-  }, []);
-
-
-  const { chain } = useNetwork();
-
-  const { switchNetwork } = useSwitchNetwork();
-
- 
+  const { data: profile, error: profileError, loading: profileLoading } = useActiveProfile();
 
   const {
     execute: login,
@@ -152,41 +38,26 @@ export function AuthContextProvider({
     isPending: isLoginPending,
   } = useWalletLogin();
 
-  const { isConnected } = useAccount();
-
-  const { data: profile, error:profileError, loading: profileLoading } = useActiveProfile();
-
+  const { execute: logout, isPending } = useWalletLogout();
 
   useEffect(() => {
-    if (!user) return;
-
-    const { id } = user;
-
-    const unsubscribeUser = onSnapshot(doc(usersCollection, id), (doc) => {
-      setUser(doc.data() as User);
-    });
-
-    const unsubscribeBookmarks = onSnapshot(
-      userBookmarksCollection(id),
-      (snapshot) => {
-        const bookmarks = snapshot.docs.map((doc) => doc.data());
-        setUserBookmarks(bookmarks);
-      }
-    );
-
-    return () => {
-      unsubscribeUser();
-      unsubscribeBookmarks();
+    const manageUser = async (): Promise<void> => {
+      setUser(profile!);
+      setLoading(profileLoading);
+      setError(profileError!);
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [user?.id]);
+    manageUser()
+  }, []);
+ 
 
-  const signInWithGoogle = async (): Promise<void> => {
+  const signInWithLens = async (): Promise<void> => {
     try {
       const walletClient = await getWalletClient();
-      await login({
-      address: walletClient.account.address,
-    });
+      if (walletClient) {
+        await login({
+          address: walletClient.account.address,
+        });
+      }
     } catch (error) {
       setError(error as Error);
     }
@@ -194,24 +65,23 @@ export function AuthContextProvider({
 
   const signOut = async (): Promise<void> => {
     try {
-      await signOutFirebase(auth);
+      logout();
     } catch (error) {
       setError(error as Error);
     }
   };
 
-  const isAdmin = user ? user.username === 'ccrsxx' : false;
+
   const randomSeed = useMemo(getRandomId, [user?.id]);
 
   const value: AuthContext = {
     user,
     error,
     loading,
-    isAdmin,
     randomSeed,
     userBookmarks,
     signOut,
-    signInWithGoogle
+    signInWithLens
   };
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
