0cde4765daf8003ff93f4ce5078fe70aa87491de
dogmanss <975965279@qq.com>
2023-08-16 14:14:36 +0800

    who follow

--- src/lib/hooks/useCollection.ts
+++ src/lib/hooks/useCollection.ts
@@ -1,13 +1,23 @@
 import { ProfileId, useExploreProfiles } from '@lens-protocol/react-web';
+import { formatAvater } from '@lib/FormatContent';
 import { User } from '@lib/types/user';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 type UseCollection<T> = {
   data: T[] | null;
   loading: boolean;
+} | {
+  data: (T & { user: UserCardProps })[] | null;
+  loading: boolean;
 };
 
-type DataWithUser<T> = UseCollection<T & { user: User }>;
+
+type UserCardProps = User & {
+  modal?: boolean;
+  follow?: boolean;
+};
+
+type DataWithUser<T> = UseCollection<T & { user: UserCardProps }>;
 
 export type UseCollectionOptions = {
   limit: number;
@@ -21,10 +31,40 @@ export function useCollection<T>(
 
   const { limit, observerId } = options ?? {};
 
+
+  const [formateList, setFormateList] = useState<UserCardProps[]>([]);
+
+ 
+
   const { data, loading } = useExploreProfiles({
     observerId,
     limit
   });
 
-  return { data, loading };
+
+  useEffect(() => {
+
+    if (data) {
+      let list: UserCardProps[] =   data.map((item => {
+      
+         return {
+            id: item.id,
+            bio: item.bio,
+            name: item.handle,
+            username: item.name,
+            photoURL: formatAvater(item.picture.original.url),
+            verified: true,
+            following: [],
+            followers: [],
+            coverPhotoURL: item.coverPicture
+          }
+      
+      }))
+
+      setFormateList(list);
+    }
+
+  }, [data])
+
+  return { data: formateList, loading };
 }
