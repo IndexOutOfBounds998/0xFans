4e72c7081cb123abd54505b10cefd7176a3e091c
dogmanss <975965279@qq.com>
2023-09-06 00:49:57 +0800

    trans

--- lingui.config.js
+++ lingui.config.js
@@ -1,6 +1,6 @@
 const { formatter } = require("@lingui/format-po")
 
-const locales = ["en-us", "nl-nl", "zh"]
+const locales = ["en-us", "zh-CN"]
 
 if (process.env.NODE_ENV !== "production") {
     locales.push("pseudo")
