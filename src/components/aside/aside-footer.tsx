4ad064164e531a127c80eb775c8d8f7dd6c7342b
dogmanss <975965279@qq.com>
2023-09-11 17:59:40 +0800

    foot

--- src/components/aside/aside-footer.tsx
+++ src/components/aside/aside-footer.tsx
@@ -1,18 +1,19 @@
 import { Switcher } from '@components/common/Switcher';
 import { Trans, t } from '@lingui/macro';
 import { useLingui } from '@lingui/react';
-
-const footerLinks = [
-  [t`Terms of Service`, 'https://0xFans.com/tos'],
-  [t`Privacy Policy`, 'https://0xFans.com/privacy'],
-  [t`Cookie Policy`, 'https://support.0xFans.com/articles/20170514'],
-  [t`Accessibility`, 'https://help.0xFans.com/resources/accessibility'],
-  [t`Ads Info`, 'https://business.0xFans.com/en/help/troubleshooting/how-0xFans-ads-work.html']
-] as const;
+import { useState } from 'react';
 
 export function AsideFooter(): JSX.Element {
-  const { i18n } = useLingui();
-  console.log('Current locale: ' + i18n.locale)
+  useLingui();
+
+  const [footerLinks] = useState<string[][]>([
+    [t`Terms of Service`, 'https://0xFans.com/tos'],
+    [t`Privacy Policy`, 'https://0xFans.com/privacy'],
+    [t`Cookie Policy`, 'https://support.0xFans.com/articles/20170514'],
+    [t`Accessibility`, 'https://help.0xFans.com/resources/accessibility'],
+    [t`Ads Info`, 'https://business.0xFans.com/en/help/troubleshooting/how-0xFans-ads-work.html']
+  ])
+
   return (
     <footer
       className='sticky top-16 flex flex-col gap-3 text-center text-sm 
